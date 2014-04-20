d55d39fd73fbf46ac01a1ca0ac7318577a94d37d
Brian Chau <me@brianchau.ca>
2014-04-19 18:53:12 -0700

    Update webpage details

--- index.php
+++ index.php
@@ -151,6 +151,20 @@ if(isset($_POST['submitted'])) {
 					<h2>Experience</h2>
 					<div class="row">
 						<div class="col-md-3">
+							<img src="./img/ata-white-small.png" alt="A Thinking Ape">
+						</div>
+						<div class="col-md-8" style="text-align: left;">
+							<h4>A Thinking Ape</h4>
+							<h5>Software Development Engineer Co-op</h5>
+							<h6>January 2014 to April 2014</h6>
+							<ul>
+								<li>I worked on a software engineering team during my time at ATA. I introduced a few new features and implemented them on the server and both Android and iOS clients.</li>
+							</ul>
+						</div>
+					</div>
+					<br /><br /><!-- NOTE NEED THIS DOUBLE BR TAG BETWEEN ELEMENTS -->
+					<div class="row">
+						<div class="col-md-3">
 							<img src="./img/ubc-white-small.png" alt="UBC">
 						</div>
 						<div class="col-md-8" style="text-align: left;">
@@ -165,20 +179,6 @@ if(isset($_POST['submitted'])) {
 						</div>
 					</div>
 					<h2>Future Internships</h2>
-					<div class="row">
-						<div class="col-md-3">
-							<img src="./img/ata-white-small.png" alt="A Thinking Ape">
-						</div>
-						<div class="col-md-8" style="text-align: left;">
-							<h4>A Thinking Ape</h4>
-							<h5>Software Development Engineer Co-op</h5>
-							<h6>January 2014 to April 2014</h6>
-							<ul>
-								<li>In 2014 I will be starting a co-op term with A Thinking Ape, a Vancouver-based mobile games company.</li>
-							</ul>
-						</div>
-					</div>
-					<br /><br /><!-- NOTE NEED THIS DOUBLE BR TAG BETWEEN ELEMENTS -->
 					<div class="row">
 						<div class="col-md-3">
 							<img src="./img/microsoft-white-small.png" alt="Microsoft">
@@ -209,8 +209,12 @@ if(isset($_POST['submitted'])) {
 							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div></div></div>
 						</div>
 						<div class="row">
+							<div class="col-xs-4">Objective-C</div>
+							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div></div></div>
+						</div>
+						<div class="row">
 							<div class="col-xs-4">Python</div>
-							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%;"></div></div></div>
+							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div></div></div>
 						</div>
 						<div class="row">
 							<div class="col-xs-4">Haskell</div>
@@ -234,7 +238,7 @@ if(isset($_POST['submitted'])) {
 						</div>
 						<div class="row">
 							<div class="col-xs-4">SQL</div>
-							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div></div></div>
+							<div class="col-xs-8"><div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%;"></div></div></div>
 						</div>
 					</div>
 				</div>
